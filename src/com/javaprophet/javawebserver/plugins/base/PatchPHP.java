1c7ea9fc94d8e898797b2e70051859150410775a
JavaProphet <max.bruce12@gmail.com>
2014-12-29 21:44:06 -0800

    Added&Removed PHP-Bridge, final strategy found. :)

--- src/com/javaprophet/javawebserver/plugins/base/PatchPHP.java
+++ src/com/javaprophet/javawebserver/plugins/base/PatchPHP.java
@@ -1,5 +1,8 @@
 package com.javaprophet.javawebserver.plugins.base;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import com.javaprophet.javawebserver.http.ContentEncoding;
 import com.javaprophet.javawebserver.http.Headers;
 import com.javaprophet.javawebserver.networking.Packet;
@@ -23,11 +26,20 @@ public class PatchPHP extends Patch {
 	
 	@Override
 	public boolean shouldProcessResponse(Headers headers, ContentEncoding ce, boolean data, byte[] response) {
-		return headers.hasHeader("Content-Type") && headers.getHeader("Content-Type").equals("application/x-php");
+		return headers.hasHeader("Content-Type") && headers.getHeader("Content-Type").value.equals("application/x-php");
 	}
 	
 	@Override
 	public byte[] processResponse(Headers headers, ContentEncoding ce, boolean data, byte[] response) {
+		ScriptEngineManager manager = new ScriptEngineManager();
+		ScriptEngine engine = manager.getEngineByExtension("php");
+		try {
+			headers.getHeader("Content-Type").value = "text/html";
+			Object s = engine.eval(new String(response));
+			System.out.println(s);
+		}catch (ScriptException ex) {
+			ex.printStackTrace();
+		}
 		return response;
 	}
 	
