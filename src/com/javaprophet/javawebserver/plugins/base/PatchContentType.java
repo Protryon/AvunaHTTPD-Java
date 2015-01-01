cfc5f2bfa2156d6db4d8c22a9bb7cfee72305ffb
JavaProphet <max.bruce12@gmail.com>
2014-12-31 17:08:58 -0800

    Forgot to add these suckers in.

---
+++ src/com/javaprophet/javawebserver/plugins/base/PatchContentType.java
@@ -0,0 +1,48 @@
+package com.javaprophet.javawebserver.plugins.base;
+
+import org.json.simple.JSONObject;
+import com.javaprophet.javawebserver.networking.Packet;
+import com.javaprophet.javawebserver.networking.packets.RequestPacket;
+import com.javaprophet.javawebserver.networking.packets.ResponsePacket;
+import com.javaprophet.javawebserver.plugins.Patch;
+
+public class PatchContentType extends Patch {
+	
+	public PatchContentType(String name) {
+		super(name);
+	}
+	
+	@Override
+	public void formatConfig(JSONObject json) {
+		
+	}
+	
+	@Override
+	public boolean shouldProcessPacket(Packet packet) {
+		return false;
+	}
+	
+	@Override
+	public void processPacket(Packet packet) {
+		
+	}
+	
+	@Override
+	public boolean shouldProcessResponse(ResponsePacket response, RequestPacket request, byte[] data) {
+		return true;
+	}
+	
+	@Override
+	public byte[] processResponse(ResponsePacket response, RequestPacket request, byte[] data) {
+		response.headers.removeHeaders("Content-Type");
+		if (data != null) {
+			response.headers.addHeader("Content-Type", response.body.getBody().type);
+		}
+		return data;
+	}
+	
+	@Override
+	public void processMethod(RequestPacket request, ResponsePacket response) {
+		
+	}
+}
---
+++ src/com/javaprophet/javawebserver/plugins/base/PatchJWSL.java
@@ -0,0 +1,91 @@
+package com.javaprophet.javawebserver.plugins.base;
+
+import java.util.HashMap;
+import org.json.simple.JSONObject;
+import com.javaprophet.javawebserver.networking.Packet;
+import com.javaprophet.javawebserver.networking.packets.RequestPacket;
+import com.javaprophet.javawebserver.networking.packets.ResponsePacket;
+import com.javaprophet.javawebserver.plugins.Patch;
+import com.javaprophet.javawebserver.util.FileManager;
+
+public class PatchJWSL extends Patch {
+	
+	public PatchJWSL(String name) {
+		super(name);
+	}
+	
+	@Override
+	public void formatConfig(JSONObject json) {
+		
+	}
+	
+	@Override
+	public boolean shouldProcessPacket(Packet packet) {
+		return false;
+	}
+	
+	@Override
+	public void processPacket(Packet packet) {
+		
+	}
+	
+	@Override
+	public boolean shouldProcessResponse(ResponsePacket response, RequestPacket request, byte[] data) {
+		return response.headers.hasHeader("Content-Type") && response.headers.getHeader("Content-Type").value.equals("application/x-jwsl") && response.body != null && data != null && data.length > 0;
+	}
+	
+	@Override
+	public byte[] processResponse(ResponsePacket response, RequestPacket request, byte[] data) {
+		String sdata = new String(data);
+		String[] ls = sdata.split(FileManager.crlf);
+		HashMap<String, Integer> codeToIndex = new HashMap<String, Integer>();
+		boolean inCode = false;
+		int cIndex = 0;
+		int lIndex = 0;
+		String cCode = "";
+		for (int i = 0; i < ls.length; i++) {
+			String line = ls[i];
+			if (line.contains("<?jwsl")) {
+				line = line.substring(line.indexOf("<?jwsl") + 6);
+				inCode = true;
+				cIndex = lIndex + line.indexOf("<?jwsl");
+			}
+			if (inCode && line.contains("?>")) { // TODO: not in quotes, etc
+				line = line.substring(0, line.indexOf("?>"));
+				cCode += line;
+				codeToIndex.put(cCode, cIndex);
+				cIndex = 0;
+				cCode = "";
+				inCode = false;
+			}
+			if (inCode) {
+				cCode += line + FileManager.crlf;
+			}
+			lIndex += ls[i].length() + FileManager.crlf.length();
+		}
+		String[] codeArray = new String[codeToIndex.size()];
+		int i = 0;
+		for (String code : codeToIndex.keySet()) {
+			codeArray[i++] = code;
+		}
+		for (int i2 = 0; i2 < codeArray.length; i2++) {
+			String code = codeArray[i2];
+			int loc = codeToIndex.get(code);
+			String before = sdata.substring(0, loc);
+			String after = sdata.substring(loc + code.length() + 9);
+			sdata = before + after;
+			for (int i3 = i2 + 1; i3 < codeArray.length; i3++) {
+				codeToIndex.put(codeArray[i3], codeToIndex.get(codeArray[i3]) - code.length() - 8);
+			}
+		}
+		// TODO: continue processing, eval, and then reoutput.
+		// TODO: proper header mod
+		response.headers.updateHeader("Content-Type", "text/html");
+		return sdata.getBytes();
+	}
+	
+	@Override
+	public void processMethod(RequestPacket request, ResponsePacket response) {
+		
+	}
+}
