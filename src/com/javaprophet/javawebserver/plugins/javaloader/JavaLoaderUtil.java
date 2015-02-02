87a73fdfdc5a40c019c0670dfd6a81890e327a7e
JavaProphet <max.bruce12@gmail.com>
2015-02-01 18:48:16 -0800

    updates

--- src/com/javaprophet/javawebserver/plugins/javaloader/JavaLoaderUtil.java
+++ src/com/javaprophet/javawebserver/plugins/javaloader/JavaLoaderUtil.java
@@ -1,17 +1,17 @@
 package com.javaprophet.javawebserver.plugins.javaloader;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JavaLoaderUtil {
-	public static String mysql_real_escape_string(java.sql.Connection link, String str) throws SQLException {
+	public static String mysql_real_escape_string(Statement stmt, String str) throws SQLException {
 		if (str == null) {
 			return "NULL";
 		}
-		
 		if (str.replaceAll("[a-zA-Z0-9_!@#$%^&*()-=+~.;:,\\Q[\\E\\Q]\\E<>{}\\/? ]", "").length() < 1) {
 			return str;
 		}
-		
 		String clean_string = str;
 		clean_string = clean_string.replaceAll("\\\\", "\\\\\\\\");
 		clean_string = clean_string.replaceAll("\\n", "\\\\n");
@@ -20,16 +20,11 @@ public class JavaLoaderUtil {
 		clean_string = clean_string.replaceAll("\\00", "\\\\0");
 		clean_string = clean_string.replaceAll("'", "\\\\'");
 		clean_string = clean_string.replaceAll("\\\"", "\\\\\"");
-		
 		if (clean_string.replaceAll("[a-zA-Z0-9_!@#$%^&*()-=+~.;:,\\Q[\\E\\Q]\\E<>{}\\/?\\\\\"' ]", "").length() < 1) {
 			return clean_string;
 		}
-		
-		java.sql.Statement stmt = link.createStatement();
-		String qry = "SELECT QUOTE('" + clean_string + "')";
-		
-		stmt.executeQuery(qry);
-		java.sql.ResultSet resultSet = stmt.getResultSet();
+		stmt.executeQuery("SELECT QUOTE('" + clean_string + "')");
+		ResultSet resultSet = stmt.getResultSet();
 		resultSet.first();
 		String r = resultSet.getString(1);
 		return r.substring(1, r.length() - 1);
