95ef6b588abed3f6ee2cadcbce621e4889c722e2
JavaProphet <max.bruce12@gmail.com>
2015-02-03 19:47:09 -0800

    Added DatabaseManager and began API for JL.

---
+++ src/com/javaprophet/javawebserver/plugins/javaloader/lib/DatabaseManager.java
@@ -0,0 +1,65 @@
+package com.javaprophet.javawebserver.plugins.javaloader.lib;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class DatabaseManager {
+	private Connection conn;
+	private ArrayList<ExtendedStatement> estmts = new ArrayList<ExtendedStatement>();
+	
+	public DatabaseManager(String driver, String ip, String db, String user, String pass) throws SQLException {
+		conn = DriverManager.getConnection("jdbc:" + driver + "://" + ip + "/" + db + "?user=" + user + "&password=" + pass);
+	}
+	
+	private static class ExtendedStatement {
+		public final Statement stmt;
+		public boolean leased = false;
+		
+		public ExtendedStatement(Statement stmt) {
+			this.stmt = stmt;
+		}
+	}
+	
+	public Statement getIndependentStatement() throws SQLException {
+		return conn.createStatement();
+	}
+	
+	public void closeStatements() throws SQLException {
+		synchronized (estmts) {
+			for (ExtendedStatement estmt : estmts) {
+				estmt.stmt.close();
+			}
+		}
+	}
+	
+	public Statement leaseStatement() throws SQLException {
+		synchronized (estmts) {
+			for (ExtendedStatement estmt : estmts) {
+				if (!estmt.leased) {
+					estmt.leased = true;
+					return estmt.stmt;
+				}
+			}
+		}
+		ExtendedStatement estmt = new ExtendedStatement(conn.createStatement());
+		estmt.leased = true;
+		synchronized (estmts) {
+			estmts.add(estmt);
+		}
+		return estmt.stmt;
+	}
+	
+	public void returnStatement(Statement stmt) {
+		synchronized (estmts) {
+			for (ExtendedStatement estmt : estmts) {
+				if (estmt.stmt.equals(stmt)) {
+					estmt.leased = false;
+				}
+			}
+		}
+	}
+	
+}
