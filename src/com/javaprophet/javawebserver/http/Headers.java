51fe9c98046ac299e047422830e8ddc0d00b1071
Luca Camphuisen <lucamasira@gmail.com>
2015-02-07 09:49:52 +0100

    JavaDoc for Headers

--- src/com/javaprophet/javawebserver/http/Headers.java
+++ src/com/javaprophet/javawebserver/http/Headers.java
@@ -3,26 +3,50 @@ package com.javaprophet.javawebserver.http;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * This class is used to store key-value properties for an HTTP header.
+ */
 public class Headers {
+
+    /**
+     * This map contains all the headers for the http header.
+     */
 	private HashMap<String, ArrayList<String>> headers = new HashMap<String, ArrayList<String>>();
-	
+
+    /**
+     * Constructor for the Headers.
+     */
 	public Headers() {
 		
 	}
-	
+
+    /**
+     * Clone the current headers into another Headers object.
+     * @return a cloned version of the current headers.
+     */
 	public Headers clone() {
 		Headers h = new Headers();
 		h.headers = (HashMap<String, ArrayList<String>>)headers.clone();
 		return h;
 	}
-	
+
+    /**
+     * Add a key-value pair to the headers.
+     * @param name the key value
+     * @param value the value to store
+     */
 	public void addHeader(String name, String value) {
 		if (!headers.containsKey(name)) {
 			headers.put(name, new ArrayList<String>());
 		}
 		headers.get(name).add(value);
 	}
-	
+
+    /**
+     * Update a header key value.
+     * @param name the key to update
+     * @param value the value to set the new key value to
+     */
 	public void updateHeader(String name, String value) {
 		if (hasHeader(name)) {
 			ArrayList<String> hdrs = getHeaders(name);
@@ -32,13 +56,22 @@ public class Headers {
 			addHeader(name, value);
 		}
 	}
-	
+
+    /**
+     * Add a header value from a http header such as "content-size: 1337"
+     * @param header the http header field.
+     */
 	public void addHeader(String header) {
 		if (header.contains(":")) {
 			addHeader(header.substring(0, header.indexOf(":")).trim(), header.substring(header.indexOf(":") + 1).trim());
 		}
 	}
-	
+
+    /**
+     * Get the headers for a specific key value.
+     * @param name the key name.
+     * @return the headers for the name.
+     */
 	public ArrayList<String> getHeaders(String name) {
 		return headers.get(name);
 	}
@@ -51,7 +84,12 @@ public class Headers {
 		}
 		return null;
 	}
-	
+
+
+    /**
+     * Remove headers from a key value.
+     * @param name the key.
+     */
 	public void removeHeaders(String name) {
 		String[] keys = headers.keySet().toArray(new String[]{});
 		for (int i = 0; i < keys.length; i++) {
@@ -60,7 +98,12 @@ public class Headers {
 			}
 		}
 	}
-	
+
+    /**
+     * Check if the headers contain the certain key.
+     * @param name the key to check
+     * @return the state if the headers contain a certain key.
+     */
 	public boolean hasHeader(String name) {
 		for (String header : headers.keySet()) {
 			if (header.equals(name)) {
@@ -69,7 +112,11 @@ public class Headers {
 		}
 		return false;
 	}
-	
+
+    /**
+     * Get all the headers.
+     * @return the hashmap of all headers.
+     */
 	public HashMap<String, ArrayList<String>> getHeaders() {
 		return headers;
 	}
