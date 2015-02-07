c8db93af045a98ba991f892ee2711dfabf1d337e
Luca Camphuisen <lucamasira@gmail.com>
2015-02-07 13:05:07 +0100

    JavaDoc ResponseGenerator

--- src/com/javaprophet/javawebserver/http/ResponseGenerator.java
+++ src/com/javaprophet/javawebserver/http/ResponseGenerator.java
@@ -10,14 +10,29 @@ import com.javaprophet.javawebserver.util.Logger;
  * Creates a http response coming from the server.
  */
 public class ResponseGenerator {
+
+    /**
+     * Our constructor
+     */
 	public ResponseGenerator() {
 		
 	}
-	
+
+    /**
+     * Dat format that isn't being used.
+     */
 	public static final SimpleDateFormat sdf = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz");
-	
+
+    /**
+     * Processes a request and fills in the ResponsePacket
+     * @param request the request
+     * @param response the response to fill in
+     * @return returns the success of handling the request.
+     */
 	public boolean process(RequestPacket request, ResponsePacket response) {
+        //Check if httpVersion is compatible
 		if (!request.httpVersion.equals("HTTP/1.1")) {
+            //NOTE: StatusCode.NEEDS_HTTP_1_1??
 			if (request.httpVersion.equals("HTTP/1.0")) {
 				request.headers.addHeader("Host", "");
 			}
@@ -33,6 +48,7 @@ public class ResponseGenerator {
 				response.headers.addHeader("Connection", request.headers.getHeader("Connection"));
 			}
 			long ah3 = System.nanoTime();
+            //Now we handle all of our patches such as php or jhtml which fill in the response.
 			if (!JavaWebServer.patchBus.processMethod(request, response)) {
 				generateDefaultResponse(response, StatusCode.NOT_YET_IMPLEMENTED);
 				JavaWebServer.fileManager.getErrorPage(response.body, request.target, StatusCode.NOT_YET_IMPLEMENTED, "The requested URL " + request.target + " via " + request.method.name + " is not yet implemented.");
@@ -52,7 +68,12 @@ public class ResponseGenerator {
 			return false;
 		}
 	}
-	
+
+    /**
+     * Generates the stausCode, httpVersion and reasonPhrase for the response
+     * @param response the response packet
+     * @param status the status to set.
+     */
 	public static void generateDefaultResponse(ResponsePacket response, StatusCode status) {
 		response.statusCode = status.getStatus();
 		response.httpVersion = "HTTP/1.1";
