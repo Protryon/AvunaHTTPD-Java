edf873da5ddc7cd175b845eb166d24c681e30bf3
JavaProphet <max.bruce12@gmail.com>
2015-02-08 00:26:38 -0800

    Added SetCookie class, precursor to Sessions.

---
+++ src/com/javaprophet/javawebserver/plugins/javaloader/lib/SetCookie.java
@@ -0,0 +1,39 @@
+package com.javaprophet.javawebserver.plugins.javaloader.lib;
+
+import com.javaprophet.javawebserver.networking.packets.RequestPacket;
+import com.javaprophet.javawebserver.networking.packets.ResponsePacket;
+
+public class SetCookie {
+	private final ResponsePacket response;
+	private final String domain;
+	
+	public SetCookie(ResponsePacket response, String domain) {
+		this.response = response;
+		this.domain = domain;
+	}
+	
+	public SetCookie(ResponsePacket response, RequestPacket request) {
+		this.response = response;
+		this.domain = request.headers.hasHeader("Host") ? request.headers.getHeader("Host") : "";
+	}
+	
+	public void setCookie(String name, String value) {
+		response.headers.addHeader("Set-Cookie", name + "=" + value + "; Path=/; Domain=" + domain);
+	}
+	
+	public void setCookie(String name, String value, int maxage) {
+		response.headers.addHeader("Set-Cookie", name + "=" + value + "; Max-Age=" + maxage + "; Path=/; Domain=" + domain);
+	}
+	
+	public void setCookie(String name, String value, int maxage, String path) {
+		response.headers.addHeader("Set-Cookie", name + "=" + value + "; Max-Age=" + maxage + "; Path=" + path + "; Domain=" + domain);
+	}
+	
+	public void setCookie(String name, String value, int maxage, String path, String domain) {
+		response.headers.addHeader("Set-Cookie", name + "=" + value + "; Max-Age=" + maxage + "; Path=" + path + "; Domain=" + domain);
+	}
+	
+	public void setCookie(String name, String value, int maxage, String path, String domain, boolean secure, boolean httponly) {
+		response.headers.addHeader("Set-Cookie", name + "=" + value + "; Max-Age=" + maxage + "; Path=" + path + "; Domain=" + domain + (secure ? "; Secure" : "") + (httponly ? "; HttpOnly" : ""));
+	}
+}
