a453965117924164b71e9a50ff7ce4bf22cb5eec
JavaProphet <max.bruce12@gmail.com>
2015-03-05 21:01:46 -0800

    Changed official version for the first time.

--- src/com/javaprophet/javawebserver/JavaWebServer.java
+++ src/com/javaprophet/javawebserver/JavaWebServer.java
@@ -30,7 +30,7 @@ import com.javaprophet.javawebserver.util.FileManager;
 import com.javaprophet.javawebserver.util.Logger;
 
 public class JavaWebServer {
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.0.1";
 	public static Config mainConfig, hostsConfig;
 	private static Config dnsConfig;
 	public static final FileManager fileManager = new FileManager();
