df7c6db6c3dfc36a445852bd7a821e4f498a1cc1
JavaProphet <max.bruce12@gmail.com>
2015-03-08 13:36:51 -0700

    Updated version number to 1.0.3.

--- src/com/javaprophet/javawebserver/JavaWebServer.java
+++ src/com/javaprophet/javawebserver/JavaWebServer.java
@@ -30,7 +30,7 @@ import com.javaprophet.javawebserver.util.FileManager;
 import com.javaprophet.javawebserver.util.Logger;
 
 public class JavaWebServer {
-	public static final String VERSION = "1.0.2";
+	public static final String VERSION = "1.0.3";
 	public static Config mainConfig, hostsConfig;
 	private static Config dnsConfig;
 	public static final FileManager fileManager = new FileManager();
