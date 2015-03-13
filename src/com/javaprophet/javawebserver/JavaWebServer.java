802ac849a535583c0de7065853e82dcf51da8487
JavaProphet <max.bruce12@gmail.com>
2015-03-12 17:07:27 -0700

    1.0.4->1.0.5

--- src/com/javaprophet/javawebserver/JavaWebServer.java
+++ src/com/javaprophet/javawebserver/JavaWebServer.java
@@ -30,7 +30,7 @@ import com.javaprophet.javawebserver.util.FileManager;
 import com.javaprophet.javawebserver.util.Logger;
 
 public class JavaWebServer {
-	public static final String VERSION = "1.0.4";
+	public static final String VERSION = "1.0.5";
 	public static Config mainConfig, hostsConfig;
 	private static Config dnsConfig;
 	public static final FileManager fileManager = new FileManager();
