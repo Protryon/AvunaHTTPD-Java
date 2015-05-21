98048583af4c43e4fca20933912a073ffaabb2a0
JavaProphet <max.bruce12@gmail.com>
2015-05-21 16:27:21 -0700

    v1.2.2

--- src/org/avuna/httpd/AvunaHTTPD.java
+++ src/org/avuna/httpd/AvunaHTTPD.java
@@ -43,7 +43,7 @@ import org.avuna.httpd.util.Logger;
 import org.avuna.httpd.util.SafeMode;
 
 public class AvunaHTTPD {
-	public static final String VERSION = "1.2.1";
+	public static final String VERSION = "1.2.2";
 	public static Config mainConfig, hostsConfig;
 	public static final FileManager fileManager = new FileManager();
 	public static final HashMap<String, String> extensionToMime = new HashMap<String, String>();
