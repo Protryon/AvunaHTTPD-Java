55a0682db2a705b27976d6ab0fe724a1fb8d1a94
JavaProphet <max.bruce12@gmail.com>
2015-06-03 18:34:52 -0700

    Rewrote EventBus to be more efficent.

--- src/org/avuna/httpd/event/EventBus.java
+++ src/org/avuna/httpd/event/EventBus.java
@@ -23,24 +23,13 @@ public class EventBus {
 			IEventReceiver[] ida = new IEventReceiver[recvs[id].length + 1];
 			System.arraycopy(recvs[id], 0, ida, 0, ida.length - 1);
 			ida[ida.length - 1] = recv;
-			recvs[id] = ida;
 			int[] idp = new int[priorities[id].length + 1];
 			System.arraycopy(priorities[id], 0, idp, 0, idp.length - 1);
 			idp[idp.length - 1] = priority;
-			priorities[id] = idp;
-		}
-	}
-	
-	public void callEvent(Event event) {
-		int id = event.getEID();
-		synchronized (recvs) {
-			if (id >= recvs.length || recvs[id].length == 0) {
-				return;
-			}
-			int[] i2r = new int[recvs[id].length];
+			int[] i2r = new int[idp.length];
 			int i2ri = 0;
-			int[] pool = new int[priorities[id].length];
-			System.arraycopy(priorities[id], 0, pool, 0, pool.length);
+			int[] pool = new int[idp.length];
+			System.arraycopy(idp, 0, pool, 0, pool.length);
 			for (int pi = 0; pi < pool.length; pi++) {
 				int li = -1;
 				int lv = Integer.MIN_VALUE;
@@ -53,7 +42,25 @@ public class EventBus {
 				i2r[i2ri++] = li;
 				pool[li] = Integer.MIN_VALUE;
 			}
+			IEventReceiver[] ida2 = new IEventReceiver[ida.length];
+			int[] idp2 = new int[idp.length];
+			int idp2i = 0;
 			for (int r : i2r) {
+				idp2[idp2i] = idp[r];
+				ida2[idp2i++] = ida[r];
+			}
+			recvs[id] = ida2;
+			priorities[id] = idp2;
+		}
+	}
+	
+	public void callEvent(Event event) {
+		int id = event.getEID();
+		synchronized (recvs) {
+			if (id >= recvs.length || recvs[id].length == 0) {
+				return;
+			}
+			for (int r = 0; r < recvs[id].length; r++) {
 				recvs[id][r].receive(this, event);
 			}
 		}
