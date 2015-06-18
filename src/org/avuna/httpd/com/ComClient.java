c2c9660f697ec4b7f0fd04be49ca7eef4027e8aa
JavaProphet <max.bruce12@gmail.com>
2015-06-17 20:53:55 -0700

    Added TCP NO_DELAY.

--- src/org/avuna/httpd/com/ComClient.java
+++ src/org/avuna/httpd/com/ComClient.java
@@ -1,18 +1,17 @@
-/*	Avuna HTTPD - General Server Applications
-    Copyright (C) 2015 Maxwell Bruce
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+/*
+ * Avuna HTTPD - General Server Applications
+ * Copyright (C) 2015 Maxwell Bruce
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 package org.avuna.httpd.com;
 
@@ -72,6 +71,7 @@ public class ComClient {
 		while (true) {
 			try {
 				cs = new Socket(ip, port);
+				cs.setTcpNoDelay(true);
 				out = new PrintStream(cs.getOutputStream());
 				out.flush();
 				scan = new DataInputStream(cs.getInputStream());
--- src/org/avuna/httpd/com/ComServer.java
+++ src/org/avuna/httpd/com/ComServer.java
@@ -1,18 +1,17 @@
-/*	Avuna HTTPD - General Server Applications
-    Copyright (C) 2015 Maxwell Bruce
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+/*
+ * Avuna HTTPD - General Server Applications
+ * Copyright (C) 2015 Maxwell Bruce
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 package org.avuna.httpd.com;
 
@@ -64,6 +63,7 @@ public class ComServer extends Thread {
 				Socket s = null;
 				try {
 					s = server.accept();
+					s.setTcpNoDelay(true);
 					if (attempts.containsKey(s.getInetAddress().getHostAddress())) {
 						if (attempts.get(s.getInetAddress().getHostAddress()) >= 4) {
 							s.close();
