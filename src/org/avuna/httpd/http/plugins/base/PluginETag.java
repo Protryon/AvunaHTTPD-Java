74ce6f9a23e14ceee33d430ca0ec262baf2f0b5a
javaprophet <max.bruce12@gmail.com>
2015-06-26 15:00:48 -0700

    Fixed PluginETag.

--- src/org/avuna/httpd/http/plugins/base/PluginETag.java
+++ src/org/avuna/httpd/http/plugins/base/PluginETag.java
@@ -1,18 +1,4 @@
-/*	Avuna HTTPD - General Server Applications
-    Copyright (C) 2015 Maxwell Bruce
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+/* Avuna HTTPD - General Server Applications Copyright (C) 2015 Maxwell Bruce This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>. */
 
 package org.avuna.httpd.http.plugins.base;
 
@@ -38,10 +24,10 @@ public class PluginETag extends Plugin {
 	@Override
 	public void receive(EventBus bus, Event event) {
 		if (event instanceof EventGenerateResponse) {
-			EventGenerateResponse egr = (EventGenerateResponse)event;
+			EventGenerateResponse egr = (EventGenerateResponse) event;
 			ResponsePacket response = egr.getResponse();
 			RequestPacket request = egr.getRequest();
-			if (!(request.parent == null && response.statusCode == 200 && (request.method == Method.GET || request.method == Method.HEAD) && response.body != null && request.body.data.length > 0)) return;
+			if ((request.parent != null || response.statusCode != 200 || (request.method != Method.GET && request.method != Method.HEAD) || response.body == null || response.body.data == null || response.body.data.length == 0)) return;
 			CRC32 crc = new CRC32();
 			crc.update(request.body.data);
 			String etag = crc.getValue() + "";// bytesToHex(md5.digest(data));
