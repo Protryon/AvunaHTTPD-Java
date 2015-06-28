83853c92f6ccab4cf214fa910ad829d5969b4b70
javaprophet <max.bruce12@gmail.com>
2015-06-27 19:39:51 -0700

    Bugfix with debug mode.

--- src/org/avuna/httpd/http/networking/RequestPacket.java
+++ src/org/avuna/httpd/http/networking/RequestPacket.java
@@ -1,5 +1,4 @@
-/*
- * Avuna HTTPD - General Server Applications Copyright (C) 2015 Maxwell Bruce This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>. */
+/* Avuna HTTPD - General Server Applications Copyright (C) 2015 Maxwell Bruce This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>. */
 
 package org.avuna.httpd.http.networking;
 
@@ -204,7 +203,7 @@ public class RequestPacket extends Packet {
 				}
 			}
 			ser.write(AvunaHTTPD.crlf.getBytes());
-			if (body != null) ser.write(body.data);
+			if (body != null && body.data != null) ser.write(body.data);
 			return ser.toByteArray();
 		}catch (Exception e) {
 			Logger.logError(e);
