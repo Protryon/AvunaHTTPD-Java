97d666b45b3fef909bab58ca970f38261395a3e6
javaprophet <max.bruce12@gmail.com>
2015-07-29 18:40:25 -0700

    Added fix for no-tls SMTP server.

--- src/org/avuna/httpd/mail/smtp/SMTPHandler.java
+++ src/org/avuna/httpd/mail/smtp/SMTPHandler.java
@@ -1,18 +1,4 @@
-/*	Avuna HTTPD - General Server Applications
-    Copyright (C) 2015 Maxwell Bruce
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+/* Avuna HTTPD - General Server Applications Copyright (C) 2015 Maxwell Bruce This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>. */
 
 package org.avuna.httpd.mail.smtp;
 
@@ -46,10 +32,14 @@ public class SMTPHandler {
 		commands.add(new SMTPCommand("starttls", 0, 100) {
 			public void run(SMTPWork focus, String line) throws IOException {
 				focus.writeLine(220, "Go ahead");
+				if (host.sslContext == null) {
+					focus.writeLine(520, "TLS not enabled!");
+					return;
+				}
 				focus.s = host.sslContext.getSocketFactory().createSocket(focus.s, focus.s.getInetAddress().getHostAddress(), focus.s.getPort(), true);
-				((SSLSocket)focus.s).setUseClientMode(false);
-				((SSLSocket)focus.s).setNeedClientAuth(false);
-				((SSLSocket)focus.s).startHandshake();
+				((SSLSocket) focus.s).setUseClientMode(false);
+				((SSLSocket) focus.s).setNeedClientAuth(false);
+				((SSLSocket) focus.s).startHandshake();
 				focus.out = new DataOutputStream(focus.s.getOutputStream());
 				focus.out.flush();
 				focus.in = new DataInputStream(focus.s.getInputStream());
@@ -64,7 +54,7 @@ public class SMTPHandler {
 			public void run(SMTPWork focus, String line) throws IOException {
 				focus.writeMLine(250, host.getConfig().getNode("domain").getValue().split(",")[0]);
 				focus.writeMLine(250, "AUTH PLAIN LOGIN");
-				focus.writeMLine(250, "STARTTLS");
+				if (host.sslContext != null) focus.writeMLine(250, "STARTTLS");
 				focus.writeLine(250, "AUTH=PLAIN LOGIN");
 				focus.state = 1;
 				focus.isExtended = true;
@@ -78,7 +68,7 @@ public class SMTPHandler {
 					line = line.substring(6).trim();
 					if (line.length() > 0) {
 						String up = new String(DatatypeConverter.parseBase64Binary(line)).substring(1);
-						String username = up.substring(0, up.indexOf(new String(new byte[]{0})));
+						String username = up.substring(0, up.indexOf(new String(new byte[] { 0 })));
 						String password = up.substring(username.length() + 1);
 						EmailAccount us = null;
 						for (EmailAccount e : host.accounts) {
