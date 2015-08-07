14d499efd71383f9e59758d2ac280396f6530589
javaprophet <max.bruce12@gmail.com>
2015-08-07 14:30:34 -0700

    Search bugfix

--- src/org/avuna/httpd/mail/imap/command/IMAPCommandSearch.java
+++ src/org/avuna/httpd/mail/imap/command/IMAPCommandSearch.java
@@ -1,18 +1,4 @@
-/*	Avuna HTTPD - General Server Applications
-    Copyright (C) 2015 Maxwell Bruce
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+/* Avuna HTTPD - General Server Applications Copyright (C) 2015 Maxwell Bruce This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>. */
 
 package org.avuna.httpd.mail.imap.command;
 
@@ -206,9 +192,15 @@ public class IMAPCommandSearch extends IMAPCommand {
 				if (emails.get(i).flags.contains("\\Flagged")) emails.remove(i--);
 			}
 		}else if (com.startsWith("uid ")) {
-			int u = Integer.parseInt(com.substring(4));
-			for (int i = 0; i < emails.size(); i++) {
-				if (emails.get(i).uid != u) emails.remove(i--);
+			try {
+				ArrayList<Email> toFetch = focus.selectedMailbox.getByIdentifier(com.substring(4));
+				for (int i = 0; i < emails.size(); i++) {
+					if (!toFetch.contains(emails.get(i))) {
+						emails.remove(i--);
+					}
+				}
+			}catch (Exception e) {
+				return false;
 			}
 		}else {
 			try {
