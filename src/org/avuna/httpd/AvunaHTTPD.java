6f778de6981e103cdaa6dc4d3fc98ddbaa0fe167
javaprophet <max.bruce12@gmail.com>
2015-08-07 22:10:24 -0700

    1.3.0 - We have some cool stuff added for more flexibility and power, By 1.4.0, hopefully we can have full C networking & a testing setup.

--- src/org/avuna/httpd/AvunaHTTPD.java
+++ src/org/avuna/httpd/AvunaHTTPD.java
@@ -46,7 +46,7 @@ import org.avuna.httpd.util.Logger;
 import org.avuna.httpd.util.SafeMode;
 
 public class AvunaHTTPD {
-	public static final String VERSION = "1.2.9";
+	public static final String VERSION = "1.3.0";
 	public static Config mainConfig, hostsConfig;
 	public static final FileManager fileManager = new FileManager();
 	public static final HashMap<String, String> extensionToMime = new HashMap<String, String>();
