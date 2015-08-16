829e43601e733b19cab25bfa81ffb72ef6267130
javaprophet <max.bruce12@gmail.com>
2015-08-15 18:49:41 -0700

    Updated FTP configs.

--- src/org/avuna/httpd/ftp/FTPConfigAccountProvider.java
+++ src/org/avuna/httpd/ftp/FTPConfigAccountProvider.java
@@ -7,55 +7,67 @@ import java.net.Inet4Address;
 import java.net.UnknownHostException;
 import org.avuna.httpd.AvunaHTTPD;
 import org.avuna.httpd.hosts.HostFTP;
+import org.avuna.httpd.util.CLib;
 import org.avuna.httpd.util.ConfigNode;
 
 public class FTPConfigAccountProvider extends FTPAccountProvider {
 	private final ConfigNode node;
-	private final int uid, gid;
 	
 	public FTPConfigAccountProvider(HostFTP host, ConfigNode node) {
 		this.node = node;
-		if (!node.containsNode("root")) node.insertNode("root", "/");
-		if (!AvunaHTTPD.windows) {
-			if (!node.containsNode("uid")) node.insertNode("uid", AvunaHTTPD.mainConfig.getNode("uid").getValue());
-			if (!node.containsNode("gid")) node.insertNode("gid", AvunaHTTPD.mainConfig.getNode("gid").getValue());
-			this.uid = Integer.parseInt(node.getNode("uid").getValue());
-			this.gid = Integer.parseInt(node.getNode("gid").getValue());
-		}else {
-			this.uid = 0;
-			this.gid = 0;
-		}
 		if (!node.containsNode("ourip")) try {
 			node.insertNode("ourip", Inet4Address.getLocalHost().getHostAddress(), "should be set to your public ip");
 		}catch (UnknownHostException e) {
 			host.logger.logError(e);
 			node.insertNode("ourip", "127.0.0.1", "should be set to your public ip");
 		}
-		if (!node.containsNode("users")) node.insertNode("users", null, "format is user=pass");
+		if (!node.containsNode("users")) node.insertNode("users", null);
+		ConfigNode users = node.getNode("users");
+		for (String nn : users.getSubnodes()) {
+			ConfigNode user = users.getNode(nn);
+			if (!user.containsNode("pass")) user.insertNode("pass", "password");
+			if (!user.containsNode("root")) user.insertNode("root", AvunaHTTPD.windows ? "C:\\" : "/");
+			if (!AvunaHTTPD.windows && !CLib.failed) {
+				if (!user.containsNode("uid")) user.insertNode("uid", CLib.getuid() + "", "simulated uid");
+				if (!user.containsNode("gid")) user.insertNode("gid", CLib.getgid() + "", "simulated gid");
+			}
+		}
 	}
 	
 	@Override
 	public boolean isValid(String user, String pass) {
 		ConfigNode users = node.getNode("users");
-		for (String un : users.getSubnodes()) {
-			if (un.equals(user)) {
-				return users.getNode(un).getValue().equals(pass);
-			}
-		}
-		return false;
+		if (!users.containsNode(user)) return false;
+		ConfigNode usn = users.getNode(user);
+		if (!usn.branching()) return false;
+		return usn.containsNode("pass") && usn.getValue("pass").equals(pass);
 	}
 	
 	@Override
 	public File getRoot(String user) {
-		return new File(node.getNode("root").getValue());
+		ConfigNode users = node.getNode("users");
+		if (!users.containsNode(user)) return null;
+		ConfigNode usn = users.getNode(user);
+		if (!usn.containsNode("root")) return null;
+		return new File(usn.getNode("root").getValue());
 	}
 	
 	public int getUID(String user) {
-		return uid;
+		if (AvunaHTTPD.windows || CLib.failed) return -1;
+		ConfigNode users = node.getNode("users");
+		if (!users.containsNode(user)) return -1;
+		ConfigNode usn = users.getNode(user);
+		if (!usn.containsNode("root")) return -1;
+		return Integer.parseInt(usn.getNode("uid").getValue());
 	}
 	
 	public int getGID(String user) {
-		return gid;
+		if (AvunaHTTPD.windows || CLib.failed) return -1;
+		ConfigNode users = node.getNode("users");
+		if (!users.containsNode(user)) return -1;
+		ConfigNode usn = users.getNode(user);
+		if (!usn.containsNode("root")) return -1;
+		return Integer.parseInt(usn.getNode("gid").getValue());
 	}
 	
 	@Override
