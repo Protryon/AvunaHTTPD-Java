b92b67a1c8e2bcee369f9cab51a73381f1dc15bf
javaprophet <max.bruce12@gmail.com>
2015-09-20 10:13:34 -0700

    Mail bugfix.

--- src/org/avuna/httpd/mail/mailbox/Mailbox.java
+++ src/org/avuna/httpd/mail/mailbox/Mailbox.java
@@ -1,18 +1,4 @@
-/*	Avuna HTTPD - General Server Applications
-    Copyright (C) 2015 Maxwell Bruce
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+/* Avuna HTTPD - General Server Applications Copyright (C) 2015 Maxwell Bruce This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>. */
 
 package org.avuna.httpd.mail.mailbox;
 
@@ -52,7 +38,7 @@ public class Mailbox {
 						if (eml != null) toFetch.add(eml);
 					}else {
 						int i = Integer.parseInt(seq) - 1;
-						if (i < emails.length) {
+						if (i < emails.length && i >= 0) {
 							Email eml = emails[i];
 							if (eml != null) toFetch.add(eml);
 						}
