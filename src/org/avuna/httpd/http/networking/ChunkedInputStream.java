bf7632e954fd2558305de09366a24601d0cba894
javaprophet <max.bruce12@gmail.com>
2015-10-14 18:35:09 -0700

    Chunked Input bugfix

--- src/org/avuna/httpd/http/networking/ChunkedInputStream.java
+++ src/org/avuna/httpd/http/networking/ChunkedInputStream.java
@@ -1,18 +1,4 @@
-/*	Avuna HTTPD - General Server Applications
-    Copyright (C) 2015 Maxwell Bruce
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+/* Avuna HTTPD - General Server Applications Copyright (C) 2015 Maxwell Bruce This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>. */
 
 package org.avuna.httpd.http.networking;
 
@@ -45,7 +31,7 @@ public class ChunkedInputStream extends InputStream {
 			if (hex.length() == 0) {
 				hex = Stream.readLine(in);
 			}
-			int l = Integer.parseInt(hex, 16);
+			int l = Integer.parseInt(hex.trim(), 16);
 			if (l == 0) {
 				ended = true;
 				return -1;
